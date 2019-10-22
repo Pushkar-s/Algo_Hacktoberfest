db1365be2df0194d7fda9e690547639d0baad0e6
Vaijanti72 <56840889+Vaijanti72@users.noreply.github.com>
2019-10-23 03:18:25 +0530

    merge sort is included
    this is the merge sort algo

--- mergesort.cpp
+++ mergesort.cpp
@@ -2,7 +2,37 @@
 using namespace std;
 
 // code for merge sort
-
+void merge(int arr[],int start,int end){
+    int middle = (start+end)/2;
+    int i=start,j = middle+1,k=0;
+    int ans[end-start+1];
+    while(i<=middle&&j<=end){
+        if(arr[i]<arr[j]){
+            ans[k] = arr[i];
+            i++;
+            k++;
+        }else{
+            ans[k] = arr[j];
+            j++;
+            k++;
+        }
+    }
+    while(i<=middle){ans[k] = arr[i];i++;k++;}
+    while(j<=end){ans[k] = arr[j]; j++; k++;}
+    k=0;
+    for(int i=start; i<=end; i++){
+        arr[i] = ans[k];
+        k++;
+    }
+}
+void merge_sort(int arr[],int start, int end){
+    if(start<end){
+        int middle = (start+end)/2;
+        merge_sort(arr,start,middle);
+        merge_sort(arr,middle+1,end);
+        merge(arr,start,end);
+    }
+}
 
 int main(){
     int arr[100],n;
