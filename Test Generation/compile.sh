822a18289d53c85d945cd37286a34edbaf6422c9
Vaijanti72 <56840889+Vaijanti72@users.noreply.github.com>
2020-10-02 18:39:42 +0530

    complete
    compilation and s.sh run

--- Test Generation/compile.sh
+++ Test Generation/compile.sh
@@ -1,2 +1,8 @@
 echo $"1 compiling gen.cpp"
-
+g++ -std=c++11 -o gen gen.cpp -Wall
+echo $"2 compiling a.cpp"
+g++ -std=c++11 -o a a.cpp -Wall
+echo $"3 compiling b.cpp"
+g++ -std=c++11 -o brute brute.cpp -Wall
+echo $"Testing.."
+bash s.sh
