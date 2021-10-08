754f5664ce786b148c592e30ada3a8b840bc92f0
Pushkar Soni <39935646+Pushkar-s@users.noreply.github.com>
2021-10-09 00:22:52 +0530

    Added something more
    something more added

--- 2021/ExchangeArguments/MinimumScalarProduct.cpp
+++ 2021/ExchangeArguments/MinimumScalarProduct.cpp
@@ -60,3 +60,26 @@ signed main(){
     }
 }
 
+
+
+
+
+/*
+    lets suppose we know which questions are we going to choose then only problem remains is choosing the order of problems
+    reward of each problem is = maxPoints[i] - (requiredTime[i]+something)*pointsPerMinute[i];
+                            reward = m1 - d1*(t1 + x) +  m2 - d2*(t1 + t2)
+    suppose we have two problems one after another 
+        reward1 = m1 - d1*(t1) + m2 - d2*(t1 + t2)
+        reward2 = m2 - d2*(t2) + m1 - d1*(t1 + t2)
+        we search for this condition,
+        when, reward1 - reward2 >= 0
+          m1 - d1*(t1) + m2 - d2*(t1 + t2)
+        -(m2 - d2*(t2) + m1 - d1*(t1 + t2)) >= 0
+        =>  d1*(t2)) >= d2*(t1)    (condition for sorting)
+        if this condition satisfies then reward1 >= reward2 implies we do not need to change the positions 
+        hence in sort function we will pass the reverse arguments.
+         
+    now we will solve the problem of choosing the problems which can be done by dp similar to knapsack.
+
+*/
+
